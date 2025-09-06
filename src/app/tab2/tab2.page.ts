eacd23f5f2f494496555763e2c01f1ff3d5ab2d4
Marly Alejandra Gutierrez Joven <marly_agutierrez@soy.sena.edu.co>
2025-09-05 22:09:14 -0500

    Crear un cliente

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+
+import { ClientesService } from '../services/clientes.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +11,35 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
+  form: any;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, 
+    private clientesService:ClientesService,
+    private alertController:AlertController) {}
 
+    ngOnInit(){
+      this.crearFormulario();
+    }
+    crearFormulario(){
+      this.form = this.fb.group({
+        documento_identidad:['', Validators.required],
+        nombre_completo:['', Validators.required],
+        telefono: ['', Validators.required],
+        direccion: ['', Validators.required],
+        email: ['', Validators.required, Validators.email]
+      })
+    }
+    guardarCliente(){
+      console.log("En este momento estoy guardando")
+      console.log(this.form.value)  
+
+      this.clientesService.guardarClientes(this.form.value).subscribe(
+        (res) => {        
+          console.log(res)
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
+    }
 }
